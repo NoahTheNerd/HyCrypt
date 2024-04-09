cbad9bf87930ce3dca29033bd2d27649e3325d0f
NoahTheNerd <ixnoahlive@gmail.com>
2024-04-09 22:57:39 +0200

    Favicon Magic!

---
+++ src/routes/player/[user]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+
+    export let data;
+
+
+
+    onMount(() => {
+        document.querySelector('#favicon')?.setAttribute('href', `https://render.skinmc.net/3d.php?user=${data.user}&vr=0&hr0&hrh=0&aa=&headOnly=true&ratio=50`)
+        document.title = data.user + ' | '+ document.title
+    })
+</script>
