eeb2fd8e44857b5df084debb5ef0fd7c7e043f49
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2024-04-12 11:11:09 +0200

    Light work on user & guild page.

--- src/routes/player/[user]/+page.svelte
+++ src/routes/player/[user]/+page.svelte
@@ -3,9 +3,19 @@
 
     export let data;
 
-
-
     onMount(() => {
+        let uuidThatWeMightUseLater;
+        fetch('https://api.mojang.com/users/profiles/minecraft/' + data.user).then(async (res) => {
+            res = await res.json()
+
+            if (!res?.id) {
+                alert('User could not be found by username!')
+                window.location.href = '/'
+            }
+
+            uuidThatWeMightUseLater = res?.id
+        })
+
         document.querySelector('#favicon')?.setAttribute('href', `https://render.skinmc.net/3d.php?user=${data.user}&vr=0&hr0&hrh=0&aa=&headOnly=true&ratio=50`)
         document.title = data.user + ' | '+ document.title
     })
